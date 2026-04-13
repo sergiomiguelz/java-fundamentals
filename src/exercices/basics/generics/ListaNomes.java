5c3b08acba6c00cc342890fd095f3538224f3ce4
sergiomiguel <cc.sergiomiguel@gmail.com>
2026-04-13 19:56:57 -0300

    feat: adiciona exercício de generics (lista de nomes)

---
+++ src/exercices/basics/generics/ListaNomes.java
@@ -0,0 +1,30 @@
+package exercices.basics.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Objetivo:
+ * Criar uma lista tipada para armazenar nomes e exibir todos os elementos.
+ */
+
+public class ListaNomes {
+    public static void main(String[] args) {
+
+        // Instanciação do ArrayList
+        List<String> listaNomes = new ArrayList<>();
+
+        // Adiciona elementos ( Strings ) na lista
+        listaNomes.add("Julia");
+        listaNomes.add("Pedro");
+        listaNomes.add("João");
+        listaNomes.add("Maria");
+        listaNomes.add("Marcos");
+
+        // Percorre e imprime os elemnentos
+        for (String nome : listaNomes){
+            System.out.println(nome);
+        }
+
+    }
+}
